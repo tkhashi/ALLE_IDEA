c00710dd73be84f0a13af353590cbe076fcf77bb
vagrant <vagrant@localhost.localdomain>
2020-08-28 03:31:09 +0000

    add sidebar html

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'users/show'
-  get 'users/edit'
+
+  root 'home#top'
+  get 'home/about'
+
+  resources :posts, only: [:index, :show, :create, :update, :destroy]
+    resources :post_commets, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  resources :users, only: [:show, :edit, :update]
   get 'users/following'
   get 'users/follower'
   get 'users/last_confirm'
   get 'users/please_signin'
   get 'users/thanks'
-  get 'home/top'
-  get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

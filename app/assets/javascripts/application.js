a0507a2e0e7442ea41ca63ad2d5990f5743596f5
vagrant <vagrant@localhost.localdomain>
2020-08-31 10:08:01 +0000

    First add gmapAPI,add drawing library

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,15 +20,16 @@
 //   elements.onclick = function(event){
 //   this.classList.toggle("is_active")
 // }
-var title = "javascriptが使えました";
-alert(title);
+
+// var title = "javascriptが使えました";
+// alert(title);
 
 
 // $(document).ready(function () {
 //   $('body').html('<h1>Hello jQuery!!</h1>');
 // });
 
-$('#slidemenu_contents .slidemenu_content[id != "contents-on-map"]').hide();
+// $('#slidemenu_contents .slidemenu_content[id != "contents-on-map"]').hide();
 
 $("#slidemenu a").on("click", function(event){
   $("#slidemenu_contents").hide();
@@ -37,3 +38,34 @@ $("#slidemenu a").on("click", function(event){
   $($(this).attr("href")).show();
   event.preventDefault();
 });
+
+function initialize() {
+  var latlng = new google.maps.LatLng(35.658704,139.745408);
+  var opts = {
+    zoom: 15,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map
+    (document.getElementById("map_canvas"), opts);
+
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
+    },
+    markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
+  });
+  drawingManager.setMap(map);
+
+}

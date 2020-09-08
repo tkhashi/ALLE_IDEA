fb0e5efd374497934f4b62f39c82917c74f4c7a7
vagrant <vagrant@localhost.localdomain>
2020-09-08 07:01:17 +0000

    modify gmap app.js.but not resolve to display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -69,60 +69,123 @@
 //   event.preventDefault();
 // });
 
-//app.html用マップ
-// function initialize() {
-//   var latlng = new google.maps.LatLng(35.658704,139.745408);
-//   var opts = {
-//     zoom: 15,
-//     center: latlng,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-//   };
-//   var map = new google.maps.Map
-//     (document.getElementById("map_canvas"), opts);
+// 閲覧用マップ
+function initialize() {
+  var latlng = new google.maps.LatLng(35.658704,139.745408);
+  var opts = {
+    zoom: 15,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map
+    (document.getElementById("map_canvas"), opts);
 
-//   var drawingManager = new google.maps.drawing.DrawingManager({
-//     drawingMode: google.maps.drawing.OverlayType.MARKER,
-//     drawingControl: true,
-//     drawingControlOptions: {
-//       position: google.maps.ControlPosition.TOP_CENTER,
-//       drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
-//     },
-//     markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
-//     circleOptions: {
-//       fillColor: '#ffff00',
-//       fillOpacity: 1,
-//       strokeWeight: 5,
-//       clickable: false,
-//       editable: true,
-//       zIndex: 1
-//     }
-//   });
-//   drawingManager.setMap(map);
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
+    },
+    markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
+  });
+  drawingManager.setMap(map);
+}
 
-// }
-
-// <---stop--->
 //投稿用マップ
-// var map;
-
 // 初期化。bodyのonloadでinit()を指定することで呼び出してます
 function init() {
 
-  // Google Mapで利用する初期設定用の変数
+  // google mapで利用する初期設定用の変数
   var latlng = new google.maps.LatLng(39, 138);
   var opts = {
     zoom: 6,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    maptypeid: google.maps.maptypeid.ROADMAP,
     center: latlng
   };
 
-  // getElementById("map")の"map"は、body内の<div id="map">より
-  var map = new google.maps.Map(document.getElementById("map"), opts);
+  // getelementbyid("map")の"map"は、body内の<div id="map">より
+  var map = new google.maps.map(document.getelementbyid("map"), opts);
   //地図クリックイベントの登録
-  google.maps.event.addListener(map, 'click', mylistener);
+  google.maps.event.addlistener(map, 'click', mylistener);
 };
 
 function mylistener(event) {
-  document.getElementById("post_lat").value= event.latLng.lat();
-  document.getElementById("post_lng").value= event.latLng.lng();
+  document.getelementbyid("post_lat").value= event.latlng.lat();
+  document.getelementbyid("post_lng").value= event.latlng.lng();
 };
+
+// //投稿用マップ2
+// function init() {
+//   var Marker;
+//   var map;
+//   var latlng = new google.maps.LatLng(35.658704,139.745408);
+//   var opts = {
+//    zoom: 15,
+//    center: latlng,
+//    mapTypeId: google.maps.MapTypeId.ROADMAP
+//   }
+ 
+//   map = new google.maps.Map
+//   (document.getElementById("map"),opts);
+ 
+//  //地図クリックイベントの登録
+//   google.maps.event.addListener(map, 'click',
+//   function(event) {
+//    if (Marker){Marker.setMap(null)};
+//    Marker = new google.maps.Marker({
+//     position: event.latLng,
+//     draggable: true,
+//     map: map
+//    });
+//    infotable(Marker.getPosition().lat(),
+//    Marker.getPosition().lng(),map.getZoom());
+//    geocode();
+//    //マーカードラッグイベントの登録
+//    google.maps.event.addListener(Marker,'dragend',
+//     function(event) {
+//     infotable(Marker.getPosition().lat(),
+//     Marker.getPosition().lng(),map.getZoom());
+//     geocode();
+//    })
+//    //地図ズームチェンジイベントの登録
+//    google.maps.event.addListener(map, 'zoom_changed',
+//     function(event) {
+//     infotable(Marker.getPosition().lat(),
+//     Marker.getPosition().lng(),map.getZoom());
+//    })
+//   })
+//  //ジオコーディング
+//   function geocode(){  var geocoder = new google.maps.Geocoder();
+//    geocoder.geocode({ 'location': Marker.getPosition()}, 
+//       function(results, status) {
+//     if (status == google.maps.GeocoderStatus.OK && results[0]){
+//       document.getElementById('id_address').innerHTML = 
+//           results[0].formatted_address.replace(/^日本, /, '');
+//     }else{
+//       document.getElementById('id_address').innerHTML = 
+//         "Geocode 取得に失敗しました";
+//      alert("Geocode 取得に失敗しました reason: "
+//             + status);
+//     }
+//    });
+//   }
+ 
+//  //HTMLtagを更新
+//   function infotable(ido,keido,level){
+//   //  document.getElementById('id_ido').innerHTML = ido;
+//   //  document.getElementById('id_keido').innerHTML = keido;
+//   //  document.getElementById('id_level').innerHTML = level;
+//     document.getelementbyid("post_lat").value= event.latlng.lat();
+//     document.getelementbyid("post_lng").value= event.latlng.lng();
+//   }
+//  }
+ 

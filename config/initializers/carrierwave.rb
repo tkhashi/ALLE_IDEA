a642bc44f25d04e7481fd18f480ad9881107c1dd
vagrant <vagrant@localhost.localdomain>
2020-09-13 20:47:47 +0000

    modify carrierwave env num

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'alleidea' # 作成したバケット名を記述
+    config.fog_directory  = 'alleidea'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
-      region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
       path_style: true
     }
 end 

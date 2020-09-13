e56eb743b1d0eda70e53de29f5488952fd268d38
vagrant <vagrant@localhost.localdomain>
2020-09-13 18:43:48 +0000

    modify act-as-taggle-on gem, to migrate

--- db/migrate/20200909212625_add_missing_unique_indices.acts_as_taggable_on_engine.rb
+++ db/migrate/20200909212625_add_missing_unique_indices.acts_as_taggable_on_engine.rb
@@ -9,6 +9,10 @@ AddMissingUniqueIndices.class_eval do
     add_index ActsAsTaggableOn.tags_table, :name, unique: true
 
     remove_index ActsAsTaggableOn.taggings_table, :tag_id if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
+    if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id) #追加
+      remove_foreign_key :taggings, :tags                      #追加
+      remove_index ActsAsTaggableOn.taggings_table, :tag_id    #追加
+    end
     remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_taggable_context_idx'
     add_index ActsAsTaggableOn.taggings_table,
               [:tag_id, :taggable_id, :taggable_type, :context, :tagger_id, :tagger_type],

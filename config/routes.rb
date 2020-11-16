19d732412e196d54bcdc61a17d25874c15cf3098
vagrant <pupodenews@gmail.com>
2020-11-16 14:49:35 +0000

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,22 @@ Rails.application.routes.draw do
     get 'followings'
     get 'followers'
   end
+<<<<<<< Updated upstream
   resources :notifications, only: :index
   get 'search' => 'users#search'
+=======
+>>>>>>> Stashed changes
 
   resources :posts, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
     resources :comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
-  
   end
+
   get 'latlngsearch/:lat/:lng' => 'posts#search', constraints: { lat: /\d+\.\d+/, lng: /\d+\.\d+/ }
+  #通知
+  resources :notifications, only: :index
+  #ユーザー･投稿検索
+  get 'search' => 'users#search'
 
 
 end
